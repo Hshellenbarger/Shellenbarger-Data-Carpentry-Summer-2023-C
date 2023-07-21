eeccbf345f82566464464a377e1b610e3904421e
Hshellenbarger <134650243+Hshellenbarger@users.noreply.github.com>
2023-07-21 10:42:42 -0400

    Update Week 7.R

--- Week 7.R
+++ Week 7.R
@@ -6,10 +6,10 @@ High<-filter(data, Rate == 200)
 Med<-filter(data, Rate == 150)
 Low<-filter(data, Rate == 100)
 DataMean<-data%>%
-  group_by(Rate,Day)%>%
+  group_by(Rate,DAP)%>%
   summarise('Average Plant Height'= mean(Height_cm))
 DataSD<-data%>%
-  group_by(Rate,Day)%>%
+  group_by(Rate,DAP)%>%
   summarise('Standard Deviation of Plant Height'= sd(Height_cm))
 
 ggplot(data = data) + 
@@ -19,29 +19,29 @@ ggplot(data=data) +
   geom_point(mapping=aes(x= Rate, y=Height_cm))
 
 ggplot(data = DataMean) + 
-  geom_point(mapping = aes(x=Rate,y=`Average Plant Height`,fill=`Day`))+
+  geom_point(mapping = aes(x=Rate,y=`Average Plant Height`,fill=`DAP`))+
   labs(x="Rate",y='Average Plant Height',Fill='Day')
 theme_classic(base_size = 18, base_family = "Helvetica") +
   theme(axis.title = element_text(size = 20))
 
-ggplot(DataMean,aes(x=DataMean$Rate,y=DataMean$`Average Plant Height`,fill=Day))+
+ggplot(DataMean,aes(x=DataMean$Rate,y=DataMean$`Average Plant Height`,fill=DAP))+
   geom_area()+
   labs(DataMean,x="Rate",y="Average Plant Height",
-       fill="Day")+
+       fill="DAP")+
   theme_classic(base_size = 18, base_family = "Helvetica") +
   theme(axis.title = element_text(size = 20))
 
 ggplot(data = DataSD) + 
-  geom_point(mapping = aes(x=Rate,y=DataSD$`Standard Deviation of Plant Height`,fill=`Day`))+
-  labs(x="Rate",y="Standard Deviation of Plant Height",Fill='Day')+
+  geom_point(mapping = aes(x=Rate,y=DataSD$`Standard Deviation of Plant Height`,fill=`DAP`))+
+  labs(x="Rate",y="Standard Deviation of Plant Height",Fill='DAP')+
 theme_classic(base_size = 18, base_family = "Helvetica") +
   theme(axis.title = element_text(size = 20))
 
 
-ggplot(DataSD,aes(x=DataSD$Rate,y=DataSD$`Standard Deviation of Plant Height`,fill=Day))+
+ggplot(DataSD,aes(x=DataSD$Rate,y=DataSD$`Standard Deviation of Plant Height`,fill=DAP))+
   geom_area()+
   labs(DataMean,x="Rate",y="Standard Deviation of Plant Height",
-       fill="Day")+
+       fill="DAP")+
   theme_classic(base_size = 18, base_family = "Helvetica") +
   theme(axis.title = element_text(size = 20))
 
@@ -55,7 +55,7 @@ ggplot(DataMean,aes(Rate))+
   
 MeanFacet<- ggplot(DataMean,aes('Average Plant Height ','Rate'))+
   geom_point()+
-  facet_grid(.~'Day')
+  facet_grid(.~'DAP')
 
  
 
